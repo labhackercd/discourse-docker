68510b2276569e3913f2dd7e00f89d850f9f8157
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-12-04 13:33:49 -0200

    Add wait postgresql script
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- start-web.sh
+++ start-web.sh
@@ -9,6 +9,15 @@ sed -i -e "s/DISCOURSE_SSO_URL/${DISCOURSE_SSO_URL}/g" config/site_settings.yml
 sed -i -e "s/DISCOURSE_SSO_SECRET/${DISCOURSE_SSO_SECRET}/g" config/site_settings.yml
 sed -i -e "s/DISCOURSE_FORCE_HOSTNAME/${DISCOURSE_FORCE_HOSTNAME}/g" config/site_settings.yml
 
+while true; do
+    PG_STATUS=`PGPASSWORD=$DISCOURSE_DB_PASSWORD psql -U $DISCOURSE_DB_USERNAME  -w -h $DISCOURSE_DB_HOST -c '\l \q' | grep postgres | wc -l`
+    if ! [ "$PG_STATUS" -eq "0" ]; then
+       break
+    fi
+    echo "Waiting Database Setup"
+    sleep 10
+done
+
 bundle exec rake db:create db:migrate
 bundle exec rake assets:precompile
 bundle exec rake create_admin
